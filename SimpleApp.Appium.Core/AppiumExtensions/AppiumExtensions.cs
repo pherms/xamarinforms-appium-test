b8c08b184fb51da976d1fffef0f55d1d062f0277
Luuk Jongebloet <luuk_jongebloet2@hotmail.com>
2021-04-15 16:42:08 +0200

    Adding appium extension

---
+++ SimpleApp.Appium.Core/AppiumExtensions/AppiumExtensions.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace SimpleApp.Appium.UITests.AppiumExtensions
+{
+    public static class AppiumExtensions
+    {
+        public static W FindElement<W>(this AppiumDriver<W> webElement, string elementName) where W : IWebElement
+        {
+            var devicePlatform = DeviceInfo.Platform.ToString();
+            switch (devicePlatform)
+            {
+                case "iOS":
+                    return webElement.FindElement(elementName);
+                case "Android":
+                    return webElement.FindElement(elementName);
+                case "UWP":
+                    return webElement.FindElementByAccessibilityId(elementName);
+                default:
+                    return default(W);
+            }
+        }
+    }
+}
