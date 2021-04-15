515caf4ca643c35a86690c849793453bb5202b1c
Pascal Herms <pherms@outlook.com>
2021-04-15 12:17:02 +0200

    changes in scripts and ignore file

--- SimpleApp/appcenter-pre-build.ps1
+++ SimpleApp/appcenter-pre-build.ps1
@@ -4,5 +4,5 @@ write-host "Starting appium server"
 start-job -scriptblock{appium -a 127.0.0.1 -p 4723 | out-null}
 
 write-host "--------- Environment Variables ---------"
-csc /?
+& csc /?
 write-host "--------- Environment Variables ---------"
