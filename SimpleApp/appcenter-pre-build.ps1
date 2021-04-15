260f7bd787e35031eb4e651fa31175b77525039b
Pascal Herms <pherms@outlook.com>
2021-04-15 12:02:55 +0200

    print csc help

--- SimpleApp/appcenter-pre-build.ps1
+++ SimpleApp/appcenter-pre-build.ps1
@@ -4,7 +4,5 @@ write-host "Starting appium server"
 start-job -scriptblock{appium -a 127.0.0.1 -p 4723 | out-null}
 
 write-host "--------- Environment Variables ---------"
-Get-ChildItem env:
-
-Get-Variable
+csc /?
 write-host "--------- Environment Variables ---------"
