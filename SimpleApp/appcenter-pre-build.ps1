ad7eb452b617c56f21b1c786944a67a6101c3e1e
Pascal Herms <pherms@outlook.com>
2021-04-15 11:47:20 +0200

    print env vars for UWP

--- SimpleApp/appcenter-pre-build.ps1
+++ SimpleApp/appcenter-pre-build.ps1
@@ -2,3 +2,9 @@
 
 write-host "Starting appium server"
 start-job -scriptblock{appium -a 127.0.0.1 -p 4723 | out-null}
+
+write-host "--------- Environment Variables ---------"
+Get-ChildItem env:
+
+Get-Variable
+write-host "--------- Environment Variables ---------"
