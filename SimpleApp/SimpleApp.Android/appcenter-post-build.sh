171fb52c77549a46c9b07934bb9b2f4a1c4828b5
Pascal Herms <pherms@outlook.com>
2021-04-15 10:31:24 +0200

    changed test directory iOS and Android

--- SimpleApp/SimpleApp.Android/appcenter-post-build.sh
+++ SimpleApp/SimpleApp.Android/appcenter-post-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Found NUnit test projects:"
-find $testDir -regex '.*SimpleApp.Appium.UITestsUWP.*\.csproj' -exec dotnet test --logger "nunit;logFileName=TestResults.xml" {} \;
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*SimpleApp.Appium.UITestsUWP.*\.csproj' -exec dotnet test --logger "nunit;logFileName=TestResults.xml" {} \;
