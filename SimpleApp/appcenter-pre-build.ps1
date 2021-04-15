00b58297ebb5244d4bff8c3b78440bc1355cf29f
Pascal Herms <pherms@outlook.com>
2021-04-15 16:19:43 +0200

    updated scripts

--- SimpleApp/appcenter-pre-build.ps1
+++ SimpleApp/appcenter-pre-build.ps1
@@ -2,7 +2,3 @@
 
 write-host "Starting appium server"
 start-job -scriptblock{appium -a 127.0.0.1 -p 4723 | out-null}
-
-write-host "--------- Environment Variables ---------"
-& csc /?
-write-host "--------- Environment Variables ---------"
