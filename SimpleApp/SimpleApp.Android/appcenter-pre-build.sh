f1a8df328246d31d60dbf8bc8f10e474f58b3675
Pascal Herms <pherms@outlook.com>
2021-04-15 16:07:38 +0200

    improved scripts

--- SimpleApp/SimpleApp.Android/appcenter-pre-build.sh
+++ SimpleApp/SimpleApp.Android/appcenter-pre-build.sh
@@ -3,3 +3,8 @@
 # Start appium server
 echo "Starting appium server"
 appium -a 127.0.0.1 -p 4723 &>/dev/null &
+echo "Appium is running in the background"
+
+echo "Running dotnet restore to generate project.assets.json file."
+dontet restore
+echo "Done running dotnet restore."
--- SimpleApp/appcenter-post-build.ps1
+++ SimpleApp/appcenter-post-build.ps1
@@ -4,25 +4,25 @@ write-output "Running unit tests."
 
 if (get-childitem -path .\SimpleApp\SimpleApp.Appium.UITestsUWP\bin -Recurse | Where-Object { $_.name -like "*UnitTests*.dll"}) {
     write-output "Unit test file found"
-    write-output (get-childitem -Path .\SimpleApp\SimpleApp.Appium.UITestsUWP\bin -Recurse -ErrorAction SilentlyContinue | where-object { $_.name -like "*UnitTests*.dll" } | select-object -Expand fullname)
+    write-output (get-childitem -Path .\SimpleApp\SimpleApp.Appium.UITestsUWP\bin -Recurse -ErrorAction SilentlyContinue | where-object { $_.name -like "*Appium*UITests*.dll" } | select-object -Expand fullname)
     # Running unit tests
-    get-childitem -Path .\SimpleApp\SimpleApp.Appium.UITestsUWP\bin -Recurse -ErrorAction SilentlyContinue | where-object { $_.name -like "*UnitTests*.dll" } | foreach-object { 
+    get-childitem -Path .\SimpleApp\SimpleApp.Appium.UITestsUWP\bin -Recurse -ErrorAction SilentlyContinue | where-object { $_.name -like "*Appium*UITests*.dll" } | foreach-object { 
         write-host "Running unit tests from file:" $_.fullname
-        & .\packages\NUnit.ConsoleRunner.3.12.0\tools\nunit3-console.exe $_.fullname 
+        start-process '.\packages\NUnit.ConsoleRunner.3.12.0\tools\nunit3-console.exe' $_.fullname 
     }
 }
 else {
     write-output "No unit test file found. Searching for unit test project file"
-    write-output (get-childitem -Path .\SimpleApp\SimpleApp.Appium.UITestsUWP -Recurse -ErrorAction SilentlyContinue | where-object { $_.name -like "*Tests*.csproj" } | select-object -Expand fullname)
+    write-output (get-childitem -Path .\SimpleApp\SimpleApp.Appium.UITestsUWP -Recurse -ErrorAction SilentlyContinue | where-object { $_.name -like "*Appium*UITests*.csproj" } | select-object -Expand fullname)
     write-output "Building NUnit test projects:"
-    get-childitem -Path .\SimpleApp\SimpleApp.Appium.UITestsUWP -Recurse -ErrorAction SilentlyContinue | where-object { $_.name -like "*Tests*.csproj" } | foreach-object {
+    get-childitem -Path .\SimpleApp\SimpleApp.Appium.UITestsUWP -Recurse -ErrorAction SilentlyContinue | where-object { $_.name -like "*Appium*UITests*.csproj" } | foreach-object {
         write-host "Building unit testfile from project:"  $_.fullname
-        start-process "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\Roslyn\csc.exe /noconfig /nowarn:1701,1702 /fullpaths /nostdlib+ /errorreport:prompt /warn:4" $_.fullname
+        start-process 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\Roslyn\csc.exe' -Argumentlist '/noconfig /nowarn:1701,1702 /fullpaths /nostdlib+ /errorreport:prompt /warn:4' $_.fullname
     }
     write-output "Running unit tests"
-    get-childitem -Path .\SimpleApp\SimpleApp.Appium.UITestsUWP\bin -Recurse -ErrorAction SilentlyContinue | where-object { $_.name -like "*Tests*.dll" } | foreach-object { 
+    get-childitem -Path .\SimpleApp\SimpleApp.Appium.UITestsUWP\bin -Recurse -ErrorAction SilentlyContinue | where-object { $_.name -like "*Appium*UITests*.dll" } | foreach-object { 
         write-host "Running unit tests from file:" $_.fullname
-        start-process ".\packages\NUnit.ConsoleRunner.3.12.0\tools\nunit3-console.exe" $_.fullname
+        start-process '.\packages\NUnit.ConsoleRunner.3.12.0\tools\nunit3-console.exe' $_.fullname
     }
 }
 
