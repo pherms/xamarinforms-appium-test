2fc9addbc77d96f4b8b0c07bc3c585a3f8422b98
Pascal Herms <pherms@outlook.com>
2021-04-15 11:03:37 +0200

    added building of tests

--- SimpleApp/SimpleApp.Android/appcenter-post-build.sh
+++ SimpleApp/SimpleApp.Android/appcenter-post-build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 echo "Found NUnit test projects:"
-find $APPCENTER_SOURCE_DIRECTORY -regex '.*SimpleApp.Appium.UITestsUWP.*\.csproj' -exec dotnet test --logger "nunit;logFileName=TestResults.xml" {} \;
+echo "Unittest dll file does not exist and needs to be build first"
+echo "Found NUnit test projects:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*SimpleApp.Appium.UITestsUWP.*\.csproj' -exec echo {} \;
+echo
+echo "Building NUnit test projects:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*SimpleApp.Appium.UITestsUWP.*\.csproj' -exec msbuild {} \;
+echo
+echo "Compiled projects to run NUnit tests:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*SimpleApp.Appium.UITestsUWP.*\.dll' -exec echo {} \;
+echo
+echo "Running NUnit tests:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*SimpleApp.Appium.UITestsUWP.*\.dll' -exec nunit3-console {} +
+echo
+# find $APPCENTER_SOURCE_DIRECTORY -regex '.*SimpleApp.Appium.UITestsUWP.*\.csproj' -exec dotnet test --logger "nunit;logFileName=TestResults.xml" {} \;
