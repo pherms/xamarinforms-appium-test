4f5b5437bf25f5210641918a01bec28cc1c8a87e
Pascal Herms <pherms@outlook.com>
2021-04-16 11:16:19 +0200

    fixed typo in pre-build scripts

--- SimpleApp/SimpleApp.Android/appcenter-pre-build.sh
+++ SimpleApp/SimpleApp.Android/appcenter-pre-build.sh
@@ -6,5 +6,5 @@ appium -a 127.0.0.1 -p 4723 &>/dev/null &
 echo "Appium is running in the background"
 
 echo "Running dotnet restore to generate project.assets.json file."
-dontet restore
+dotnet restore
 echo "Done running dotnet restore."
