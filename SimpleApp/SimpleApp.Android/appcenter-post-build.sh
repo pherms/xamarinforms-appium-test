94ec50436df70bb899ead44354c6c115248dcb8b
Pascal Herms <pherms@outlook.com>
2021-04-16 15:42:11 +0200

    added reinstall of appium.webdriver

--- SimpleApp/SimpleApp.Android/appcenter-post-build.sh
+++ SimpleApp/SimpleApp.Android/appcenter-post-build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+echo "Reinstalling Appium.Webdriver"
+dotnet add "$APPCENTER_SOURCE_DIRECTORY/SimpleApp/SimpleApp.Appium.UITestsUWP/SimpleApp.Appium.UITestsUWP.csproj" package Appium.WebDriver --version 4.3.1
+echo "Done reinstalling Appium.Webdriver"
 
 echo "Found NUnit test projects:"
 echo "Unittest dll file does not exist and needs to be build first"
 echo "Found NUnit test projects:"
 find $APPCENTER_SOURCE_DIRECTORY -regex '.*SimpleApp.Appium.UITestsUWP.*\.csproj' -exec echo {} \;
-echo
-echo "Building NUnit test projects:"
-find $APPCENTER_SOURCE_DIRECTORY -regex '.*SimpleApp.Appium.UITestsUWP.*\.csproj' -exec dotnet restore {} \;
 echo
 echo "Building NUnit test projects:"
 find $APPCENTER_SOURCE_DIRECTORY -regex '.*SimpleApp.Appium.UITestsUWP.*\.csproj' -exec msbuild {} \;
