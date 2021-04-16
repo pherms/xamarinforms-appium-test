c5d3b49d7bad516d2a19700799efb6b190a2eed4
Pascal Herms <pherms@outlook.com>
2021-04-16 11:30:06 +0200

    Added search for package.assests.json

--- SimpleApp/appcenter-pre-build.sh
+++ SimpleApp/appcenter-pre-build.sh
@@ -8,3 +8,6 @@ echo "Appium is running in the background."
 echo "Running dotnet restore to generate project.assets.json file."
 dotnet restore
 echo "Done running dotnet restore."
+echo "Searching for project.assets.json:"
+find "$APPCENTER_SOURCE_DIRECTORY\SimpleApp\SimpleApp.iOS" -regex '.*project.assets.*\.json' -exec echo {} \;
+echo
