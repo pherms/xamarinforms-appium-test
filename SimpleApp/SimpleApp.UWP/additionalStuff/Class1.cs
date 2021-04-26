973b19f2f35f3b9e727bcb0211dfd90986cc98e5
Pascal Herms <pherms@outlook.com>
2021-04-26 16:48:28 +0200

    changed class1

--- SimpleApp/SimpleApp.UWP/additionalStuff/Class1.cs
+++ SimpleApp/SimpleApp.UWP/additionalStuff/Class1.cs
@@ -8,30 +8,12 @@ using Android.Views;
 
 namespace SimpleApp.UWP.additionalStuff
 {
-    class Class1
-    {
-    protected bool SetProperty<T>(ref T backingStore, T value,
 
-    [CallerMemberName] string propertyName = "",
-                Action onChanged = null)
-
-
-        protected override void OnCreate(Bundle savedInstanceState)
-
+        class Hello
         {
-
-            TabLayoutResource = Resource.Layout.Tabbar;
-
-            ToolbarResource = Resource.Layout.Toolbar;
-
-            base.OnCreate(savedInstanceState);
-
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-
-            LoadApplication(new App());
-
+            static void Main(string[] args)
+            {
+                System.Console.WriteLine("Hello World!");
+            }
         }
-    }
 }
